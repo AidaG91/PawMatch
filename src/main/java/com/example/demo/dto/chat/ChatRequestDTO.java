460e4d4deba09b466213e3333b7d5340bcd987d0
AidaG91 <aida.garmus@gmail.com>
2025-08-26 09:26:59 +0200

    DTOs + Services

---
+++ src/main/java/com/example/demo/dto/chat/ChatRequestDTO.java
@@ -0,0 +1,17 @@
+package com.example.demo.dto.chat;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class ChatRequestDTO {
+
+    @NotNull
+    private Long matchId;
+
+    @NotNull
+    private Long usuario1Id;
+
+    @NotNull
+    private Long usuario2Id;
+}
